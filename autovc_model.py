c626a473f2eddb8b684dbf0ea02499eb6fc828bd
Alexander <alexander.wei@tufts.edu>
2023-02-24 02:40:04 +0000

    training loop and preprocessor

--- autovc_model.py
+++ autovc_model.py
@@ -14,7 +14,6 @@ scale_linear = 1
 class LinearNorm(layers.Layer):
     def __init__(self, out_dim, bias=True, scale=1.):
         super(LinearNorm, self).__init__()
-
         init = VarianceScaling(
             scale=scale, mode="fan_avg", distribution="uniform"
         )
@@ -22,16 +21,13 @@ class LinearNorm(layers.Layer):
             units=out_dim, activation=None, use_bias=bias,
             kernel_initializer = init
         )
-
     def call(self, inputs):
         return self.linear_layer(inputs)
-
 
 class ConvNorm(layers.Layer):
     def __init__(self, out_channels, kernel_size=1,
                  stride=1, padding="same", bias=True, scale=1.):
         super(ConvNorm, self).__init__()
-
         init = VarianceScaling(
             scale=scale, mode="fan_avg", distribution="uniform"
         )
@@ -40,10 +36,8 @@ class ConvNorm(layers.Layer):
             strides=stride, padding=padding, kernel_initializer = init,
             data_format="channels_last"
         )
-
     def call(self, inputs):
         return self.conv(inputs)
-
 
 class Encoder(layers.Layer):
     def __init__(self, dim_neck, dim_emb, t_freq):
@@ -53,7 +47,6 @@ class Encoder(layers.Layer):
         self.freq = t_freq
 
         convolutions = []
-
         for i in range(3):
             conv_layer = Sequential(
                 [ ConvNorm(512,
@@ -63,49 +56,44 @@ class Encoder(layers.Layer):
                   layers.BatchNormalization() ]
             )
             convolutions.append(conv_layer)
-
         self.convolutions = convolutions
-
         self.lstm1 = layers.Bidirectional(
             layers.CuDNNLSTM(units=dim_neck, return_sequences=True),
-            merge_mode='concat')
+            merge_mode='concat'
+        )
         self.lstm2 = layers.Bidirectional(
             layers.CuDNNLSTM(units=dim_neck, return_sequences=True),
-            merge_mode='concat')
-
+            merge_mode='concat'
+        )
     def call(self, x, c_org):
         x = layers.GaussianNoise(.05)(x)
         x = layers.Concatenate(axis=-1)([x,c_org])
-
         for conv in self.convolutions:
             x = layers.ReLU()(
-                conv(x))
+                conv(x)
+            )
         outputs = self.lstm1(x)
         outputs = self.lstm2(outputs)
         outputs = layers.Reshape((128,self.dim_neck*2,1))(outputs)
         out_forward = outputs[:,:,:self.dim_neck]
         out_backward = outputs[:,:,self.dim_neck:]
-
         codes = []
         for i in range(0, 128, self.freq):
             codes.append(
                 layers.Concatenate(axis=1)([
                     out_forward[:,i+self.freq-1,:],
-                    out_backward[:,i,:]]))
-
+                    out_backward[:,i,:]])
+            )
         codes = layers.Concatenate(axis=-1)(codes)
-
         codes = layers.Permute((2,1))(codes)
         return codes
-
 
 class Decoder(layers.Layer):
     def __init__(self, dim_pre):
         super(Decoder, self).__init__()
-
         self.lstm1 = layers.CuDNNLSTM(
-            units=dim_pre, return_sequences=True)
-
+            units=dim_pre, return_sequences=True
+        )
         convolutions = []
         for i in range(3):
             conv_layer = Sequential(
@@ -118,25 +106,22 @@ class Decoder(layers.Layer):
             convolutions.append(conv_layer)
 
         self.convolutions = convolutions
-
         self.lstm2 = Sequential(
             [ layers.CuDNNLSTM(units=1024, return_sequences=True),
               layers.CuDNNLSTM(units=1024,return_sequences=True) ]
         )
-
         self.linear_projection = LinearNorm(80)
 
     def call(self, inputs):
         x = self.lstm1(inputs)
         for conv in self.convolutions:
             x = layers.ReLU()(
-                conv(x))
+                conv(x)
+            )
         outputs = self.lstm2(x)
 
         decoder_output = self.linear_projection(outputs)
-
         return decoder_output
-
 
 class Postnet(layers.Layer):
     def __init__(self):
@@ -170,15 +155,12 @@ class Postnet(layers.Layer):
                            scale=scale_linear),
                   layers.BatchNormalization() ])
         )
-
     def call(self, x):
         for i in range(len(self.convolutions) - 1):
             x = self.convolutions[i](x)
 
         x = self.convolutions[-1](x)
-
         return x
-
 
 class AutoVC(Model):
     def __init__(self, inputs, name="AutoVC", dim_neck=16, dim_emb=32, dim_freq=16, **av):
@@ -186,7 +168,6 @@ class AutoVC(Model):
         self.Enc = Encoder(dim_neck,dim_emb,dim_freq)
         self.Dec = Decoder(dim_pre=512)
         self.postnet = Postnet()
-
         self.zoom = layers.RandomZoom(
             height_factor= (-.1,.1),
             width_factor = (-.05, .05))
@@ -194,11 +175,9 @@ class AutoVC(Model):
         self.image0_tracker = tf.keras.metrics.Mean(name="image0")
         self.postnet_tracker = tf.keras.metrics.Mean(name="postnet")
         self.code_tracker = tf.keras.metrics.Mean(name="code")
-
         self.loss_tracker = tf.keras.metrics.Mean(name='loss')
 
         self.dim_freq = dim_freq
-
         # loss function weights
         try:
             self.lam1 = av['lam1']
@@ -215,17 +194,14 @@ class AutoVC(Model):
         c_targ_ =  inputs['embeds_']
 
         x = layers.Reshape((128,80))(x)
-
         c_org = layers.Reshape((32,))(c_org_)
         c_targ = layers.Reshape((32,))(c_targ_)
-
         c_org = layers.RepeatVector(128)(c_org)
         c_targ = layers.RepeatVector(128)(c_targ)
+        
         codes_src_ = self.Enc(x,c_org)
-
         codes_src_ = layers.UpSampling1D(self.dim_freq)(codes_src_)
         codes_targ = layers.Concatenate(axis=-1)([codes_src_, c_targ])
-
         y_targ = self.Dec(codes_targ)
         y_postnet_targ = self.postnet(y_targ) + y_targ
 
@@ -239,7 +215,6 @@ class AutoVC(Model):
 
     def train_step(self, data):
         x, y = data
-
         with tf.GradientTape() as tape:
             y_pred = self(x, training=True)
 
--- train.py
+++ train.py
@@ -9,9 +9,8 @@ from .data_loader import Sample_Loader, shuffled_Sample_Loader
 from .autovc_model import AutoVC
 
 def main():
-    model = AutoVC(inputs = XGENERATOR)
+    model = AutoVC(inputs = shuf_XGENERATOR)
     model.compile(optimizer=tf.keras.optimizers.Adam(.0001))
-
     model.fit(shuf_XGENERATOR,   epochs=300000, shuffle=False)
 
 if __name__ == "__main__":
@@ -19,16 +18,13 @@ if __name__ == "__main__":
     PWD = os.getcwd()
     with open( os.path.join(PWD, "bin", "embs.pk"), 'rb') as fi:
         emb_dict = pk.load(fi)
-
     with open( os.path.join(PWD, "bin", "spects.pk"), 'rb') as fi:
         spectros = pk.load(fi)
-
     with open( os.path.join(PWD, "bin", "valspects.pk"), 'rb') as fi:
         valspectros = pk.load(fi)
-
     speakers = list(emb_dict.keys())
+
     # generate training sequence
     XGENERATOR = Sample_Loader(spectros, emb_dict, batch_size=2)
     shuf_XGENERATOR = shuffled_Sample_Loader(XGENERATOR)
-
     main()
